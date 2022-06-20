1ac85f54d5fd91440fc8273183496d995d8f53ff
mr nim <mrnim@nimtechnology.com>
2022-06-21 00:28:24 +0700

    step build image

--- vars/ciIntegrateCd.groovy
+++ vars/ciIntegrateCd.groovy
@@ -4,6 +4,13 @@ def call(Map parameters = [:]) {
             disableConcurrentBuilds()
         ]
     )
+
+    /*
+    * Get project name from SCM URL
+    * Project name must not contain '.' character
+    */
+    this.project = scm.getUserRemoteConfigs()[0].getUrl().tokenize('/')[3].split("\\.")[0].toLowerCase()
+
     node('jenkins-vm') {
         stage('Checkout'){
             checkout([
@@ -15,12 +22,13 @@ def call(Map parameters = [:]) {
             ])  
         }
         stage('Build images') {
+            docker.build("${this.project}:${env.BUILD_ID}")
             // sh "docker build -t ${IMAGE_NAME}:latest ."
             // sh "docker build -t ${IMAGE_NAME}:${GIT_HASH} ."
-            script {
-                app_latest = docker.build nameImage + ":latest"
-                app_version = docker.build nameImage + ":$GIT_HASH"
-            }
+            // script {
+            //     app_latest = docker.build nameImage + ":latest"
+            //     app_version = docker.build nameImage + ":$GIT_HASH"
+            // }
         }
     }
 }
